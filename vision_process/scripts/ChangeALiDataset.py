f6edabdd62d15dd741fb519a0fc72c518a2517b8
elevenjiang1 <1206413225@qq.com>
2020-09-24 17:40:31 +0800

    完成了ChagneALiDataset.py文件的代码整合

--- vision_process/scripts/ChangeALiDataset.py
+++ vision_process/scripts/ChangeALiDataset.py
@@ -1,9 +1,19 @@
 ## !/usr/bin/env python
 ## -*- coding: utf-8 -*-
 """
-这个文件夹对ocrtoc_materials中的models进行更改,对里面的所有visual mesh的颜色更改为统一的HSV颜色,从而方便生成mask
-另外,还进行了points.xyz的生成,从而适配YCB数据集
+文件说明:
+ALi数据集中,需要生成一套HSV外表的图片,从而可以生成分割图片
+与此同时,还需要生成目标的points.xyz文件,从而进行神经网络识别
 
+
+运行逻辑:
+1.获取所有物体的名称
+2.重新生成所有物体的颜色
+3.获取所有场景中物体的尺寸
+4.读取obj文件生成对应的xyz文件
+5.生成只包含points.xyz的文件
+
+更改记录:
 Log:2020.9.8:
     这里面指定一个models路径,然后更改里面所有物体的visual mesh.
     存在一些小细节不算完善,比如object_list中缺少了prism(这个是缺少了颜色外表,之后颜色识别的时候可能会出问题),以及object_list这里面直接写死
@@ -13,6 +23,9 @@ Log:2020.9.10:
     加入了prism这个类,让他也占据一个HSV的值,之后遇到了识别问题再去单独处理这个,先不管那一部分
     加入了generate_xyz_file,根据obj文件生成points.xyz(且目标都是collision.obj)
     加入了add_scale函数,对color_index进行了添加,最后一行是scale,目前只解决1-,2-,3-三个场景的问题
+
+log:2020.9.24:
+    加入了新的物体,将所有物体从60个添加到了80个,进行了代码的重新书写
 """
 import sys
 import os
@@ -24,108 +37,57 @@ import xml.etree.ElementTree as ET
 
 python_path=os.path.dirname(__file__)
 
-class Read_YCB:
-    """
-    这里面用于读取YCB格式的数据集,然后生成对应的点
-    """
-    def __init__(self):
-        self.classes_file_path=python_path+"/classes.txt"
-        self.root=python_path+"/../ALi_dataset"
-        self.objects_points={}
-        self.objects_names=['a_cups', 'a_lego_duplo', 'a_toy_airplane', 'adjustable_wrench', 'b_cups', 'b_lego_duplo', 'b_toy_airplane', 'banana', 'bleach_cleanser', 'bowl', 'bowl_a', 'c_cups', 'c_lego_duplo', 'c_toy_airplane', 'cracker_box', 'cup_small', 'd_cups', 'd_lego_duplo', 'd_toy_airplane', 'e_cups', 'e_lego_duplo', 'e_toy_airplane', 'extra_large_clamp', 'f_cups', 'f_lego_duplo', 'flat_screwdriver', 'foam_brick', 'fork', 'g_cups', 'g_lego_duplo', 'gelatin_box', 'h_cups', 'hammer', 'i_cups', 'j_cups', 'jenga', 'knife', 'large_clamp', 'large_marker', 'master_chef_can', 'medium_clamp', 'mug', 'mustard_bottle', 'nine_hole_peg_test', 'pan_tefal', 'phillips_screwdriver', 'pitcher_base', 'plate', 'potted_meat_can', 'power_drill', 'prism', 'pudding_box', 'rubiks_cube', 'scissors', 'spoon', 'sugar_box', 'tomato_soup_can', 'tuna_fish_can', 'wood_block']
-        # self.get_objects_points(self.classes_file_path)#这里进行self.objects_points的填充
-
-    def read_obj_from_file(self,obj_path,see_file=False):
-        """
-        通过解析obj源文件,对里面的v开头的行进行解析,读取一个obj文件,返回N*3个点的np矩阵
-        @param obj_path:obj文件路径
-        @param see_file:是否进行点云可视化
-        @return:返回这个文件对应的点云
-        """
-        data=open(obj_path)
-        points=[]
-
-        for line in data.readlines():
-            if line.startswith('#'):
-                continue
-            values=line.split()
-            if not values:#如果没有值(空行),也跳过
-                continue
-
-            if values[0]=='v':
-                v=[ float(x) for x in values[1:4]]
-                points.append(v)
-
-        points=np.array(points)
-        if see_file:
-            pcd=o3d.geometry.PointCloud()
-            pcd.points=o3d.utility.Vector3dVector(points)
-            o3d.visualization.draw_geometries([pcd])#可视化
-
-        return points
-
-    def get_objects_points(self,classes_file_path):
-        """
-        基于class_file中的所有类别,读取对应类别的points.xyz文件
-        @param classes_file_path: classes.txt文件路径
-        @return:
-        """
-        class_file = open(classes_file_path)
-        while 1:
-            class_input = class_file.readline()
-            object_name=class_input[:-1]
-            if not class_input:
-                break
-            points_file = open('{0}/models/{1}/collision_meshes/points.xyz'.format(self.root,object_name))
-            self.objects_points[object_name] = []
-            while 1:
-                input_line = points_file.readline()
-                if not input_line:
-                    break
-                input_line = input_line[:-1].split(' ')
-                self.objects_points[object_name].append([float(input_line[0]), float(input_line[1]), float(input_line[2])])
-            self.objects_points[object_name] = np.array(self.objects_points[object_name])
-            points_file.close()
-
-    def get_objects_points_from_obj(self,classes_file_path):
-        """
-        基于classes_file更新self.ojbects_points,但是这里面保存的是mesh文件,不是np.array文件
-        @param classes_file_path:
-        @return:
-        """
-        class_file = open(classes_file_path)
-        while 1:
-            class_input = class_file.readline()
-
-            if not class_input:
-                break
-            points=o3d.io.read_triangle_mesh('{0}/models/{1}/collision_meshes/collision.obj'.format(self.root, class_input[:-1]))
-
-            self.objects_points[class_input[:-1]]=points
-
 class Change_Dataset:
-    def __init__(self,models_path=None):
+    def __init__(self,material_path=None):
         """
-        这个类用于阿里给定的数据集进行变更
+        这个类用于对阿里给定的数据集进行变更
         1:将Models中物体的外表变换为固定颜色用于获得语义信息
         2:读取每一个物体的obj文件,获取物体的点,生成对应的xyz文件
+        3:获取所有场景的.world文件,获取他们对应的scale从而进行重建
         """
-        #models路径
-        if models_path is None:
-            self.models_path="/home/elevenjiang/Desktop/ocrtoc_materials/models_change"
+        #1:指定对应路径
+        #ocrtoc_materials路径(主目录)
+        if material_path is None:
+            self.material_path="/home/elevenjiang/Desktop/ocrtoc_materials"
         else:
-            self.models_path=models_path
+            self.material_path=material_path
 
-        self.scenes="/home/elevenjiang/Desktop/ocrtoc_materials/scenes"
+        self.models_path=self.material_path+"/"+"models_change"#models路径
+        self.scenes_path=self.material_path+"/"+"scenes"#scenes路径
 
-        #生成物体名称
-        self.objects_names=['a_cups', 'a_lego_duplo', 'a_toy_airplane', 'adjustable_wrench', 'b_cups', 'b_lego_duplo', 'b_toy_airplane', 'banana', 'bleach_cleanser', 'bowl', 'bowl_a', 'c_cups', 'c_lego_duplo', 'c_toy_airplane', 'cracker_box', 'cup_small', 'd_cups', 'd_lego_duplo', 'd_toy_airplane', 'e_cups', 'e_lego_duplo', 'e_toy_airplane', 'extra_large_clamp', 'f_cups', 'f_lego_duplo', 'flat_screwdriver', 'foam_brick', 'fork', 'g_cups', 'g_lego_duplo', 'gelatin_box', 'h_cups', 'hammer', 'i_cups', 'j_cups', 'jenga', 'knife', 'large_clamp', 'large_marker', 'master_chef_can', 'medium_clamp', 'mug', 'mustard_bottle', 'nine_hole_peg_test', 'pan_tefal', 'phillips_screwdriver', 'pitcher_base', 'plate', 'potted_meat_can', 'power_drill', 'prism', 'pudding_box', 'rubiks_cube', 'scissors', 'spoon', 'sugar_box', 'tomato_soup_can', 'tuna_fish_can', 'wood_block']
+        #从classes.txt中获取物体名称
+        self.objects_names=self.get_objects_name()
 
         #生成颜色编码方式,H采用180度分为30个档(6为间隔),S有85,179,255,V默认为200
         self.HSV_list,self.BGR_list=self.generate_color_list()
 
-        #获取obj文件
-        self.read_YCB=Read_YCB()
+    ####################################生成classes.txt####################################
+    def generate_classes_txt(self):
+        """
+        读取models文件下的所有文件夹,生成对应的名称
+        注意里面的.gitignore需要手动删除
+        :return:
+        """
+        class_names=os.listdir(self.models_path)
+        class_names.sort()
+
+        file=open("classes.txt",'w')
+        for class_name in class_names:
+            file.write(class_name+"\n")
+        file.close()
+
+    def get_objects_name(self):
+        """
+        这里面进行此目录中classes.txt的文件更新
+        :return:
+        """
+        file=open("classes.txt",'r')
+        class_names=file.readlines()
+        objects_name=[]
+        for class_name in class_names:
+            class_name=class_name[:-1]#去掉\n
+            objects_name.append(class_name)
+        return objects_name
 
     ###################################更改物体颜色部分的代码######################################
     def generate_color_list(self,v_value=200):
@@ -142,9 +104,8 @@ class Change_Dataset:
         BGR_list=[]
         s_index=[85,170,255]
         v=v_value
-        h_number=math.ceil(len(self.objects_names)/len(s_index))
+        h_number=int(math.ceil(float(len(self.objects_names))/len(s_index)))#确保生成的h_number是超过object_names的
         h_range=int(180/h_number)
-
         for s in s_index:
             for i in range(h_number):
                 HSV_list.append((int(i*h_range),s,v))
@@ -250,6 +211,7 @@ class Change_Dataset:
     def change_color(self,debug=False):
         """
         指定路径之后,可以改变所有物体的颜色,得到对应目标
+        prism中没有对应的png,不进行对应图片的生成
         @param debug: debug为True时查看生成效果,False时是更改图片
         @return:
         """
@@ -259,7 +221,7 @@ class Change_Dataset:
 
         #1.读取每一个文件,并生成对应的参数
         for i,object_name in enumerate(self.objects_names):
-            print("Now is changeing object:",object_name)
+            print("Now is changeing object: {}".format(object_name))
             if object_name=="table":
                 continue
 
@@ -278,8 +240,9 @@ class Change_Dataset:
                 if name[:-4]!=".dae":
                     picture_dir=visual_meshes_dir+"/"+name
                     break
+
             if picture_dir is None:
-                print("The objet",object_name,"have problem")
+                print("[Error] The objet",object_name,"has problem,please check")
                 sys.exit()
             testure_map=cv.imread(picture_dir)
 
@@ -334,6 +297,99 @@ class Change_Dataset:
             write_thing="{}:{},{},{},{},{},{},\n".format(object_name,b,g,r,h,s,v)
             color_index.write(write_thing)
         color_index.close()
+        print("Already change the object color and generate the color_index.txt")
+
+    ####################################查询所有scale部分####################################
+    def add_scale(self):
+        """
+        在1-,2-,3-场景中,scale都是相同的,但是4,5两个场景中的物体却是不一定的,因此,目前只基于前3个场景进行尺寸生成
+        所有的index变换成csv文件,从而可以更好的进行索引
+        @return:
+        """
+        #生成一个空的dict用于scale的存储
+        scale_dict={}
+        for true_name in self.objects_names:
+            scale_dict[true_name]=[]
+
+        #寻找所有的scene文件
+        files=os.listdir(self.scenes_path)
+        for file in files:
+            if file[0]=='4' or file[0]=='5':
+                continue
+            world_file=os.path.join(self.scenes_path,file)+"/input.world"
+            xml_root=ET.parse(world_file).getroot()
+            for child_1 in xml_root.findall('world/model'):
+                gazebo_name=child_1.attrib['name']
+                if gazebo_name == "ground_plane" or gazebo_name == "table":
+                    continue
+
+                #1:读取物体真实名称
+                true_name=None
+                for child_2 in child_1.findall('link/collision/geometry/mesh/uri'):
+                    uri=child_2.text
+                    uri=uri[8:]
+                    stop=uri.find('/')
+                    true_name=uri[:stop]
+                    # if true_name=="prism":
+                    #     #这里面更新一下prism的东西
+                    #     pass
+
+                if true_name is None:
+                    print("[Warning] {} file {} don't have true_name".format(world_file,gazebo_name))
+
+                #2:获取物体scale
+                scale=None
+                for child_2 in child_1.findall('link/collision/geometry/mesh/scale'):
+                    scale_info=child_2.text
+                    scale_info=scale_info.split(' ')
+                    if scale_info[0]!=scale_info[1]:
+                        print("[Warning] {} scale_info rate is not all same".format(world_file))
+                    scale=float(scale_info[0])
+                    break
+                if scale is None:
+                    print("[Warning] {} file {} don't have scale".format(world_file,scale))
+
+                #3:检索已经存在的scale,看看是否会不同
+                if scale in scale_dict[true_name]:
+                    continue
+                else:
+                    if len(scale_dict[true_name])==0:
+                        scale_dict[true_name].append(scale)
+                    else:
+                        print("Find {} object has differennt scale,it is in :{} ".format(true_name,world_file))
+                        print("The origin is {}".format(scale_dict[true_name]))
+                        print("The new is ".format(scale))
+
+                        scale_dict[true_name].append(scale)
+
+        #输出scale_dict,查看是否进行保存
+        for object in scale_dict:
+            print("object name is:{},its scale is:{}".format(object,scale_dict[object]))
+            if len(scale_dict[object])>1:
+                print("!!!!!!!This Object Scale is not only one!!!!!!!")
+
+
+        temp=raw_input("Generate all_index.txt?\n          ---y for yes,else for no")
+        print temp
+        if temp !='y':
+            return
+
+        print("Begin to Generate all_index.txt...")
+        #4:最后在color_index的txt中进行更多的添加
+        color_index=open("color_index.txt",'r')
+        all_index=open('all_index.txt','w')
+        data=color_index.readlines()
+
+        #4.1:对于每个物体进行处理,保存他们对应的scale
+        for line in data:
+            name=line.split(':')[0]
+            if len(scale_dict[name])==0:
+                save_scale=1.0
+            else:
+                save_scale=scale_dict[name][0]
+            save_info=line[:-1]+"{},\n".format(save_scale)
+            print("all_index file has write:  {}".format(save_info))
+            all_index.write(save_info)
 
     ####################################生成xyz点的代码####################################
     def generate_xyz_file(self):
@@ -341,7 +397,7 @@ class Change_Dataset:
         读取每个文件的.obj文件,然后在同目录下生成一个对应的xyz文件
         @return:
         """
-        #1:读取所有物体的scale
+        #1:读取所有物体的scale(仅适用于1-3场景)
         scale_list={}
         all_index=open("all_index.txt")
         for line in all_index:
@@ -371,13 +427,13 @@ class Change_Dataset:
                 sys.exit()
 
             print("********************************")
-            print("now the i is",i)
+            print("now the {} object".format(i))
             obj_dir=meshes_dir+"/"+obj_name
             tri_mesh=o3d.io.read_triangle_mesh(obj_dir)
             point_cloud=tri_mesh.sample_points_poisson_disk(3500)
             np_points=np.asarray(point_cloud.points)
             np_points=np_points*scale_list[object_name]
-            print("this object scale is ",scale_list[object_name])
+            print("this object scale is :{}".format(scale_list[object_name]))
 
             #1.2:生成xyz文件
             xyz_path=meshes_dir+"/points.xyz"
@@ -387,95 +443,18 @@ class Change_Dataset:
 
             print("already generate {}'s points.xyz".format(object_name))
 
-    ####################################生成classes.txt####################################
-    def generate_classes_txt(self):
-        file=open("classes.txt",'w')
-        for name in self.objects_names:
-            file.write(name+"\n")
-        file.close()
 
-    ####################################查询所有scale部分####################################
-    def add_scale(self):
-        """
-        在1-,2-,3-场景中,scale都是相同的,但是4,5两个场景中的物体却是不一定的,因此,目前只基于前3个场景进行尺寸生成
-        所有的index变换成csv文件,从而可以更好的进行索引
-        @return:
-        """
-        #生成一个空的dict用于scale的存储
-        scale_dict={}
-        for true_name in self.objects_names:
-            scale_dict[true_name]=[]
+####################使用样例#######################
+def example():
+    change_Dataset=Change_Dataset()#定义更改数据集的类
+    change_Dataset.generate_classes_txt()#生成所有类别txt的文件
+    change_Dataset.change_color()#更改所有图片的颜色
+    change_Dataset.add_scale()#索引所有场景的scale,从而生成统一的尺寸
+    change_Dataset.generate_xyz_file()#生成points.xyz文件
 
-        #寻找所有的scene文件
-        files=os.listdir(self.scenes)
-        for file in files:
-            if file[0]=='4' or file[0]=='5':
-                continue
-
-            world_file=os.path.join(self.scenes,file)+"/input.world"
-            xml_root=ET.parse(world_file).getroot()
-            for child_1 in xml_root.findall('world/model'):
-                gazebo_name=child_1.attrib['name']
-                if gazebo_name == "ground_plane" or gazebo_name == "table":
-                    continue
-
-                #1:读取物体真实名称
-                true_name=None
-                for child_2 in child_1.findall('link/collision/geometry/mesh/uri'):
-                    uri=child_2.text
-                    uri=uri[8:]
-                    stop=uri.find('/')
-                    true_name=uri[:stop]
-                    if true_name=="prism":
-                        #这里面更新一下prism的东西
-                        pass
-
-                if true_name is None:
-                    print("[Warning] {} file {} don't have true_name".format(world_file,gazebo_name))
-
-                #2:获取物体scale
-                scale=None
-                for child_2 in child_1.findall('link/collision/geometry/mesh/scale'):
-                    scale_info=child_2.text
-                    scale_info=scale_info.split(' ')
-                    if scale_info[0]!=scale_info[1]:
-                        print("[Warning] {} scale_info rate is not all same".format(world_file))
-                    scale=float(scale_info[0])
-                    break
-                if scale is None:
-                    print("[Warning] {} file {} don't have scale".format(world_file,scale))
-
-                #3:检索已经存在的scale,看看是否会不同
-                if scale in scale_dict[true_name]:
-                    continue
-                else:
-                    if len(scale_dict[true_name])==0:
-                        scale_dict[true_name].append(scale)
-                    else:
-                        print("发现不同的{}的不同scale,在{}文件中".format(true_name,world_file))
-                        print("原始的是",scale_dict[true_name])
-                        print("新的是:",scale)
-                        scale_dict[true_name].append(scale)
-
-        #4:最后在color_index的txt中进行更多的添加
-        color_index=open("color_index.txt",'r')
-        all_index=open('all_index.txt','w')
-        data=color_index.readlines()
-        
-        #4.1:对于每个物体进行处理,保存他们对应的scale
-        for line in data:
-            name=line.split(':')[0]
-            if len(scale_dict[name])==0:
-                save_scale=1.0
-            else:
-                save_scale=scale_dict[name][0]
-            save_info=line[:-1]+"{},\n".format(save_scale)
-            print("要保存的内容为:",save_info)
-            all_index.write(save_info)
-
-def temp():
-    # model_path="/home/elevenjiang/Documents/Project/IROS_Pick/Code/OCRTOC_software_package/vision_process/ALi_dataset/models"
+def main():
     change_Dataset=Change_Dataset()
+    change_Dataset.generate_xyz_file()
 
 if __name__ == '__main__':
-    temp()
+    main()
